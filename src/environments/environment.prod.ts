ea13ba339f9d961c4e5a9d063eba5769bc881c4a
carlos.cary2 <gary7412@hotmail.com>
2021-09-08 14:21:54 -0400

    change api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  // APIEndpoint: "http://www.docsacademicos.com/backend"
-  APIEndpoint: "https://review-doc-api.herokuapp.com"
+  APIEndpoint: "http://www.docsacademicos.com/backend"
+  // APIEndpoint: "https://review-doc-api.herokuapp.com" para pruebas
 };
